6f60f3ed50fbcd54a690ca015d9926dc7f16daf7
blVck49 <adebiyitope49@gmail.com>
2020-07-27 13:05:50 +0100

    second commit

--- network.tf
+++ network.tf
@@ -1,100 +1,77 @@
-resource "aws_vpc" "def-vpc"{
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "PROD-vpc" {
+    cidr_block = "{var.PROD-vpc}"
     tags = {
-        Name = "def-vpc"
+        Name = "PROD-vpc"
     }
 }
-resource "aws_subnet" "PUB-subnet1"{
-    vpc_id = "${aws_vpc_def-vpc.id}"
-    cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "PUB-subnet1" {
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
+    cidr_block = "{var.PUB-subnet1}"
     tags = {
         Name = "PUB-subnet1"
     }
 }
 resource "aws_subnet" "PUB-subnet2"{
-    vpc_id = "${aws_vpc_def-vpc.id}"
-    cidr_block = "10.0.0.1/24"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
+    cidr_block = "{var.PUB-subnet2}"
     tags = {
         Name = "PUB-subnet2"
     }
 }
 resource "aws_subnet" "PRIV-subnet1"{
-    vpc_id = "${aws_vpc_def-vpc.id}"
-    cidr_block = "10.0.0.2/24"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
+    cidr_block = "{var.PRIV-subnet1}"
     tags = {
         Name = "PRIV-subnet1"
     }
 }
 resource "aws_subnet" "PRIV-subnet2"{
-    vpc_id = "${aws_vpc_def-vpc.id}"
-    cidr_block = "10.0.0.3/24"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
+    cidr_block = "{var.PRIV-subnet2}"
     tags = {
         Name = "PRIV-subnet2"
     }
 }
 resource "aws_internet_gateway" "IGW"{
-    vpc_id = "${aws_vpc_def-vpc.id}"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
     tags = {
         Name = "IGW"
     }
 }
-resource "aws_network_interface" "one"{
-    vpc = true
-    subnet_id = "${aws_subnet.PUB-subnet1.id}"
-    associate_with_private_ip = "10.0.0.10/24"
-}
-resource "aws_network_interface" "two"{
-    vpc = true
-    subnet_id = "${aws_subnet.PUB-subnet2.id}"
-    associate_with_private_ip = "10.0.0.11/24"
-}
-resource "aws_nat_gateway" "Nat_GW1"{
-    allocation_id = "${aws_eip.nat.id}"
-    subnet_id = "${aws_subnet.PUB-subnet1.id}"
-}
-resource "aws_nat_gateway" "Nat_GW2"{
-    allocation_id = "${aws_eip.nat.id}"
-    subnet_id = "${aws_subnet.PUB-subnet2.id}"
-}
 resource "aws_route_table" "r1"{
-    vpc_id = "${aws_vpc.def-vpc.id}"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
     route {
-        cidr_block = "10.0.0.0/24"
-        gateway_id = "${aws_internet_gateway.IGW.id}"
+        cidr_block = "${var.PUB-subnet1}"
         tags = {
             Name = "r1"
         }
     }
 }
 resource "aws_route_table" "r2"{
-    vpc_id = "${aws_vpc.def-vpc.id}"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
     route {
-        cidr_block = "10.0.0.1/24"
-        gateway_id = "${aws_internet_gateway.IGW.id}"
+        cidr_block = "${var.PUB-subnet2}"
         tags = {
             Name = "r2"
         }
     }
 }
 resource "aws_route_table" "r3"{
-    vpc_id = "${aws_vpc.def-vpc.id}"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
     route {
-        cidr_block = "10.0.0.2/24"
-        gateway_id = "${aws_nat_gateway.Nat_GW1}"
+        cidr_block = "${var.PRIV-subnet1}"
         tags = {
             Name = "r3"
         }
     }
 }
 resource "aws_route_table" "r4"{
-    vpc_id = "${aws_vpc.def-vpc.id}"
+    vpc_id = "${aws_vpc.PROD-vpc.id}"
     route {
-        cidr_block = "10.0.0.3/24"
-        gateway_id = "${aws_nat_gateway.Nat_GW2}"
+        cidr_block = "${var.PRIV-subnet2}"
         tags = {
             Name = "r4"
         }
     }
-
 }
 
--- variable.tf
+++ variable.tf
@@ -1,3 +1,18 @@
 variable "region" {
     default = "us-west-2"
 }
+variable "def-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+variable "PUB-subnet1" {
+    cidr_block = "10.0.0.0/24"
+}
+variable "PUB-subnet2" {
+    cidr_block = "10.0.0.1/24"
+}
+variable "PRIV-subnet1" {
+    cidr_block = "10.0.0.2/24"
+}
+variable "PRIV-subnet2" {
+    cidr_block = "10.0.0.3/24"
+}
